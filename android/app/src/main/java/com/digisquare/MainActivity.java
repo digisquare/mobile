76063bae590fbee0ab50914a802a23fab6f93121
Damien Varron <damien@varron.fr>
2016-05-03 17:51:32 +0200

    add fabric

--- android/app/src/main/java/com/digisquare/MainActivity.java
+++ android/app/src/main/java/com/digisquare/MainActivity.java
@@ -5,11 +5,22 @@ import com.oblador.vectoricons.VectorIconsPackage;
 import com.AirMaps.AirPackage;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.smixx.fabric.FabricPackage;
 
 import java.util.Arrays;
 import java.util.List;
 
+import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+
 public class MainActivity extends ReactActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -36,6 +47,7 @@ public class MainActivity extends ReactActivity {
     @Override
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
+            new FabricPackage(this),
             new MainReactPackage(),
             new VectorIconsPackage(),
             new AirPackage()
