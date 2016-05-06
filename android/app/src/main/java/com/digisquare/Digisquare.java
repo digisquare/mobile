03c79e8cc5886f8f1f6d87b63cb0209e7bedc165
Damien Varron <damien@varron.fr>
2016-05-06 18:13:48 +0200

    update notification icon

--- android/app/src/main/java/com/digisquare/Digisquare.java
+++ android/app/src/main/java/com/digisquare/Digisquare.java
@@ -1,8 +1,10 @@
 package net.digisquare;
 
 import android.app.Application;
+import android.support.v7.app.NotificationCompat;
 
 import com.urbanairship.UAirship;
+import com.urbanairship.push.notifications.DefaultNotificationFactory;
 
 public class Digisquare extends Application {
 
@@ -13,6 +15,19 @@ public class Digisquare extends Application {
         UAirship.takeOff(this, new UAirship.OnReadyCallback() {
             @Override
             public void onAirshipReady(UAirship airship) {
+                // Create a customized default notification factory
+                DefaultNotificationFactory notificationFactory;
+                notificationFactory = new DefaultNotificationFactory(getApplicationContext());
+
+                // Custom notification icon
+                notificationFactory.setSmallIconId(R.drawable.ic_notification);
+
+                // The accent color for Android Lollipop+
+                notificationFactory.setColor(NotificationCompat.COLOR_DEFAULT);
+
+                // Set the factory on the PushManager
+                airship.getPushManager().setNotificationFactory(notificationFactory);
+
                 airship.getPushManager().setUserNotificationsEnabled(true);
             }
         });
