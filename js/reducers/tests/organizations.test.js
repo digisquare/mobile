2d84f70dff9ce01a62442de378d2f3b4d031f190
Damien Varron <damien@varron.fr>
2016-05-23 14:37:12 +0200

    test reducers

---
+++ js/reducers/tests/organizations.test.js
@@ -0,0 +1,75 @@
+import { expect } from 'chai'
+import organizations from '../organizations'
+import * as organizationsActions from '../../actions/organizations.js';
+
+describe('organizations reducer', () => {
+  it('should return the initial state', () => {
+    expect(organizations(undefined, {})).to.deep.equal({});
+  });
+
+  it('should handle FETCH_ORGANIZATIONS_REQUEST', () => {
+    expect(
+      organizations([], {
+        type: organizationsActions.FETCH_ORGANIZATIONS_REQUEST,
+        edition: 9,
+      })
+    ).to.deep.equal(
+      {
+        9: {
+          isFetching: true,
+          error: false,
+          items: [],
+          lastUpdated: undefined,
+        },
+      },
+    )
+  });
+
+  it('should handle FETCH_ORGANIZATIONS_SUCCESS', () => {
+    const fetchedOrganizations = {
+      Organization: {
+        id: 1,
+      },
+    };
+    const receivedAt = +new Date();
+    expect(
+      organizations([], {
+        type: organizationsActions.FETCH_ORGANIZATIONS_SUCCESS,
+        edition: 9,
+        organizations: [fetchedOrganizations],
+        receivedAt: receivedAt,
+      })
+    ).to.deep.equal(
+      {
+        9: {
+          isFetching: false,
+          error: false,
+          items: [fetchedOrganizations],
+          lastUpdated: receivedAt,
+        },
+      },
+    )
+  });
+
+  it('should handle FETCH_ORGANIZATIONS_FAILURE', () => {
+    const error = 'An error message';
+    const receivedAt = +new Date();
+    expect(
+      organizations([], {
+        type: organizationsActions.FETCH_ORGANIZATIONS_FAILURE,
+        edition: 9,
+        error: error,
+        receivedAt: receivedAt,
+      })
+    ).to.deep.equal(
+      {
+        9: {
+          isFetching: false,
+          error: error,
+          items: [],
+          lastUpdated: receivedAt,
+        },
+      },
+    )
+  });
+});
