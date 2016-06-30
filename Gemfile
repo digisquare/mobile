20e6d092ba4d03d5ece45c72775114e3f1c76926
Damien Varron <damien@varron.fr>
2016-06-30 11:39:55 +0200

    add fastlane ios deployment

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+gem 'match'
+gem 'gym'
+gem 'pilot'
+gem 'fastlane'
--- ios/digisquare/AppDelegate.m
+++ ios/digisquare/AppDelegate.m
@@ -43,7 +43,11 @@
    * on the same Wi-Fi network.
    */
 
-  jsCodeLocation = [NSURL URLWithString:@"http://192.168.0.12:8081/index.ios.bundle?platform=ios&dev=true"];
+  #ifdef DEBUG
+    jsCodeLocation = [NSURL URLWithString:@"http://192.168.0.12:8081/index.ios.bundle?platform=ios&dev=true"];
+  #else
+    jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  #endif
 
   /**
    * OPTION 2
