701cb8704e874864fbc13afd0325f008a5baad9e
Damien Varron <damien@varron.fr>
2016-07-09 14:30:36 +0200

    fix android build for react-native 0.29.0

--- android/app/src/main/java/net/digisquare/app/MainActivity.java
+++ android/app/src/main/java/net/digisquare/app/MainActivity.java
@@ -7,21 +7,7 @@ import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.List;
 
-import android.os.Bundle;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.airbnb.android.react.maps.MapsPackage;
-import com.crashlytics.android.Crashlytics;
-import io.fabric.sdk.android.Fabric;
-import com.smixx.fabric.FabricPackage;
-import com.geektime.reactnativeonesignal.ReactNativeOneSignalPackage;
-
 public class MainActivity extends ReactActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
-    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -30,29 +16,5 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "digisquare";
-    }
-
-    /**
-     * Returns whether dev mode should be enabled.
-     * This enables e.g. the dev menu.
-     */
-    @Override
-    protected boolean getUseDeveloperSupport() {
-        return BuildConfig.DEBUG;
-    }
-
-    /**
-     * A list of packages used by the app. If the app uses additional views
-     * or modules besides the default ones, add more packages here.
-     */
-    @Override
-    protected List<ReactPackage> getPackages() {
-        return Arrays.<ReactPackage>asList(
-            new FabricPackage(this),
-            new MainReactPackage(),
-            new VectorIconsPackage(),
-            new MapsPackage(),
-            new ReactNativeOneSignalPackage(this)
-        );
     }
 }
---
+++ android/app/src/main/java/net/digisquare/app/MainApplication.java
@@ -0,0 +1,52 @@
+package net.digisquare.app;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+import com.smixx.fabric.FabricPackage;
+import com.geektime.reactnativeonesignal.ReactNativeOneSignalPackage;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    Fabric.with(this, new Crashlytics());
+  }
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    protected boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+        new FabricPackage(),
+        new MainReactPackage(),
+        new VectorIconsPackage(),
+        new MapsPackage(),
+        new ReactNativeOneSignalPackage()
+      );
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+      return mReactNativeHost;
+  }
+}
